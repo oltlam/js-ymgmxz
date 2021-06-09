22acbd685641a011a34932a78fe2e69d358e0b03
oltlam <matuzalen789@gmail.com>
2021-06-09 01:13:12 -0400

    2 files modified

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Import stylesheets
-import './style.css';
+//import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+//appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
 function testing(){
   document.getElementById("testb").innerText = "ar";
@@ -11,5 +11,5 @@ function testing(){
 }
 function changetexto(){
 
-  document.getElementById("texto1").innerText = document.getElementById("texto").innerText;
+  document.getElementById("texto").innerText = document.getElementById("texto1").innerText;
 }
